cc1ebc1aa792aa45cbf0aa07a7abd3928b9d3ffb
MetaEMK <Jan-Benecke@t-online.de>
2023-05-30 11:59:59 +0200

    update todo-comment config

--- lua/metaemk/packer.lua
+++ lua/metaemk/packer.lua
@@ -73,17 +73,7 @@ use {
   requires = {'nvim-tree/nvim-web-devicons'}
 }
 
-use {
-  "folke/todo-comments.nvim",
-  requires = "nvim-lua/plenary.nvim",
-  config = function()
-    require("todo-comments").setup {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-    }
-  end
-}
+use { "folke/todo-comments.nvim" }
 
 use('RRethy/vim-illuminate')
 
