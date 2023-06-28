38132480669969ab0b50efebe5ff31bee62341ce
MetaEMK <Jan-Benecke@t-online.de>
2023-06-28 09:24:12 +0200

    change theme to nightfox

--- lua/metaemk/packer.lua
+++ lua/metaemk/packer.lua
@@ -13,17 +13,18 @@ return require('packer').startup(function(use)
 	  requires = { {'nvim-lua/plenary.nvim'} }
   }
 
-use({
-  'rose-pine/neovim',
-  as = 'rose-pine',
-  config = function()
-      vim.cmd('colorscheme rose-pine')
-  end
-})
+--use({
+--  'rose-pine/neovim',
+--  as = 'rose-pine',
+--  config = function()
+--      vim.cmd('colorscheme rose-pine')
+--  end
+--})
 
 -- use ('Mofiqul/dracula.nvim')
-use ('simrat39/symbols-outline.nvim')
 --use 'navarasu/onedark.nvim'
+use "EdenEast/nightfox.nvim"
+use ('simrat39/symbols-outline.nvim')
 use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
 use('nvim-treesitter/playground')
 use('theprimeagen/harpoon')
