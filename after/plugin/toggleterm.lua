eb4fde037678f4fe00b90480b3fdae242e551f08
MetaEMK <jan-benecke@t-online.de>
2023-09-29 02:13:09 +0200

    adding gobang as toggleterm plugin

--- after/plugin/toggleterm.lua
+++ after/plugin/toggleterm.lua
@@ -21,4 +21,27 @@ function _lazygit_toggle()
   lazygit:toggle()
 end
 
+local gobang = Terminal:new({
+  cmd = "gobang",
+  -- dir = "git_dir",
+  direction = "float",
+  float_opts = {
+    border = "double",
+  },
+  -- function to run on opening the terminal
+  on_open = function(term)
+    vim.cmd("startinsert!")
+    vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", {noremap = true, silent = true})
+  end,
+  -- function to run on closing the terminal
+  on_close = function(term)
+    vim.cmd("startinsert!")
+  end,
+})
+
+function _gobang_toggle()
+  gobang:toggle()
+end
+
 vim.api.nvim_set_keymap("n", "<leader>ag", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<leader>ad", "<cmd>lua _gobang_toggle()<CR>", {noremap = true, silent = true})
