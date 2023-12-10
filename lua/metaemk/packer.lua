54b830d7a5841e2c8c95c0a3936bac49ffaf9f51
MetaEMK <>
2023-12-10 12:25:54 +0100

    install markdown-preview

--- lua/metaemk/packer.lua
+++ lua/metaemk/packer.lua
@@ -111,4 +111,6 @@ use { 'numToStr/Comment.nvim' }
 use { "zbirenbaum/copilot.lua" }
 use { "github/copilot.vim"}
 
+use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
+
 end)
