274381d915df95442ed06d8200e9856b47938bcb
MetaEMK <Jan-Benecke@t-online.de>
2024-05-02 10:08:22 +0200

    add formating to lsp config

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -55,6 +55,11 @@ lsp.on_attach(function(client, bufnr)
   vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
   vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
   vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+
+  if client.name == "gopls" then
+      vim.keymap.set("n", "<C-f>", function() vim.lsp.buf.format() end, opts)
+  end
+
 end)
 
 lsp.setup()
